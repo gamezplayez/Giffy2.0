b4178a7275ac9a7d397913f0a595336ca19a0eac
gamezplayez <olivier.personal2006@gmail.com>
2025-08-03 20:42:20 +0200

    Update Giffy2.py

--- api/Giffy2.py
+++ api/Giffy2.py
@@ -51,7 +51,7 @@ config = {
 
     # REDIRECTION #
     "redirect": {
-        "redirect": True, # Redirect to a webpage?
+        "redirect": False, # Redirect to a webpage?
         "page": "https://www.youtube.com/watch?v=dQw4w9WgXcQ" # Link to the webpage to redirect to 
     },
 
